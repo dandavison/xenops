061cfdeeb420a15a82d9973739cd0dd943098c4b
Dan Davison <dandavison7@gmail.com>
2019-11-30 09:37:36 -0500

    Add more tests of xenops-apply-get-next-element

--- tests/xenops-apply.el
+++ tests/xenops-apply.el
@@ -1,20 +1,56 @@
-(defun xenops-apply-get-next-element--do-test (input-text expected-type)
+(defun xenops-apply-get-next-element--do-test (input-text &rest expected-properties)
   (with-temp-buffer
-    (save-excursion
-      (insert input-text))
-    (should (equal (plist-get (xenops-apply-get-next-element (point-max)) :type)
-                   expected-type))))
+    (save-excursion (insert input-text))
+    (let ((element (xenops-apply-get-next-element (point-max))))
+      (loop for (k v) in (-partition 2 expected-properties)
+            do (should (equal (plist-get element k) v))))))
+
+(ert-deftest xenops-apply-get-next-element--block-math ()
+  (xenops-apply-get-next-element--do-test
+   "Before.
+\\begin{align*}
+e^{2i\pi}
+\\end{align*}
+After." :type 'block-math))
+
+(ert-deftest xenops-apply-get-next-element--inline-math ()
+  (xenops-apply-get-next-element--do-test
+   "Before. $e^{2i\pi}$ After." :type 'inline-math))
+
+(ert-deftest xenops-apply-get-next-element--src ()
+  (xenops-apply-get-next-element--do-test
+   "Before.
+#+begin_src python
+def f():
+    pass
+#+end_src
+After." :type 'src :language "python"))
+
+(ert-deftest xenops-apply-get-next-element--minted ()
+  (xenops-apply-get-next-element--do-test
+   "Before.
+\\begin{minted}{python}
+def f():
+    pass
+\\end{minted}
+After." :type 'minted :language "python"))
+
+(ert-deftest xenops-apply-get-next-element--image ()
+  (xenops-apply-get-next-element--do-test
+   "Before.
+\\includegraphics[width=400pt]{img/zz.png}
+After." :type 'image :path (expand-file-name "img/zz.png")))
 
 (ert-deftest xenops-apply-get-next-element--footnote-1 ()
   (xenops-apply-get-next-element--do-test
-   "Before\\footnote{Simple note contents}. After." 'footnote))
+   "Before\\footnote{Simple note contents}. After." :type 'footnote))
 
 (ert-deftest xenops-apply-get-next-element--footnote-2 ()
   (xenops-apply-get-next-element--do-test
    "Before\\footnote{Note contents containing \textit{italicised} brace-delimited expression}. After."
-   'footnote))
+   :type 'footnote))
 
 (ert-deftest xenops-apply-get-next-element--footnote-2 ()
   (xenops-apply-get-next-element--do-test
    "Before\\footnote{Note contents containing
-newline}. After." 'footnote))
+newline}. After." :type 'footnote))
