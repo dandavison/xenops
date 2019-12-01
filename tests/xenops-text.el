d0609ce1bcdf5b0c97c8dc2f635fbac2356e1661
Dan Davison <dandavison7@gmail.com>
2019-12-01 09:11:54 -0500

    Add xenops-text test coverage

--- tests/xenops-text.el
+++ tests/xenops-text.el
@@ -1,56 +1,92 @@
-(ert-deftest xenops-text-prettify-symbols ()
+(ert-deftest xenops-text-prettify-symbols-native-replacement ()
+  "An entry in  `prettify-symbols-alist' set outside xenops should be honored."
+  (let ((prettify-symbols-alist '(("replacement-specified-outside-xenops" . ?⚡))))
+    (with-temp-buffer
+      (save-excursion (insert "replacement-specified-outside-xenops"))
+      (xenops-mode)
+      (font-lock-fontify-buffer)
+      (should (plist-get (text-properties-at (point)) 'prettify-symbols-start))
+      (should (equal
+               (xenops-text-composition-to-displayed-string
+                (plist-get (text-properties-at (point)) 'composition))
+               "⚡")))))
+
+(ert-deftest xenops-text-prettify-symbols-xenops-replacement ()
+  "Test replacement by a single character."
   (with-temp-buffer
     (save-excursion (insert "\\grad"))
     (xenops-mode)
     (font-lock-fontify-buffer)
-    (should (plist-get (text-properties-at (point)) 'prettify-symbols-start))))
+    (should (plist-get (text-properties-at (point)) 'prettify-symbols-start))
+    (should (equal
+             (xenops-text-composition-to-displayed-string
+              (plist-get (text-properties-at (point)) 'composition))
+             "∇"))))
 
 (ert-deftest xenops-text-prettify-symbols-string-replacement ()
+  "Test replacement by a multi-character string."
   (with-temp-buffer
     (save-excursion (insert "\\begin{abstract}"))
     (xenops-mode)
     (font-lock-fontify-buffer)
     (should (plist-get (text-properties-at (point)) 'prettify-symbols-start))
     (should (equal
-             (xenops-text-composition-to-string
+             (xenops-text-composition-to-displayed-string
               (plist-get (text-properties-at (point)) 'composition))
              "Abstract."))))
 
 (ert-deftest xenops-text-prettify-symbols-regexp-replacement-1 ()
+  "Test replacement by regexp captured text."
   (with-temp-buffer
     (save-excursion (insert "\\textit{To be italicised}"))
     (xenops-mode)
     (font-lock-fontify-buffer)
     (should (plist-get (text-properties-at (point)) 'prettify-symbols-start))
     (should (equal
-             (xenops-text-composition-to-string
+             (xenops-text-composition-to-displayed-string
               (plist-get (text-properties-at (point)) 'composition))
              "To be italicised"))))
 
 (ert-deftest xenops-text-prettify-symbols-regexp-replacement-2 ()
+  "Test replacement by regexp captured text employing a formatting function."
   (with-temp-buffer
     (save-excursion (insert "\\subsection*{Section title}"))
     (xenops-mode)
     (font-lock-fontify-buffer)
     (should (plist-get (text-properties-at (point)) 'prettify-symbols-start))
     (should (equal
-             (xenops-text-composition-to-string
+             (xenops-text-composition-to-displayed-string
               (plist-get (text-properties-at (point)) 'composition))
              "§§ Section title"))))
 
-(defun xenops-text-composition-to-string (composition)
-  "Return string corresponding to composition text property. When
-  the text \"Abstract.\" is displayed, the composition text
-  property has a value like
+(defun xenops-text-composition-to-displayed-string (composition)
+  "Return string corresponding to composition text property.
+
+When the displayed text is the string \"Abstract.\", the
+composition text property has a value like
 
 \(...
-      [16 65 63 98 63 115 63 116 63 114 63 97 63 99 63 116 63 46]\)
+      [16 65 63 98 63 115 63 116 63 114 63 97 63 99 63 116 63 46]\).
 
-After stripping out the interleaved character 63 this is the
-string \"Abstract.\".  I'm not sure what the 63 is and I'm not
-sure what the leading 16 is and sometimes it's a list and
-sometimes a vector etc; this may be very fragile."
-  (let ((list-of-chars (append (car (last composition)) nil)))
-    (concat
-     (loop for (- char) in (-partition 2 list-of-chars)
-           collecting char))))
+After stripping out the interleaved characters 16 and 63 this is
+the string \"Abstract.\".
+
+When the character ⚡ is displayed, the composition text property
+  has a value like
+
+\(\(36 . 9889\)\).
+
+This may be fragile; this data structure is documented to not be
+part of the public API."
+  (let* ((displayed-text (car (last composition))))
+    (if (vectorp displayed-text)
+        (setq displayed-text (append displayed-text nil)))
+    (cond
+     ((listp (cdr displayed-text))
+      (concat
+       (loop for (_ char) in (-partition 2 displayed-text)
+             collecting char)))
+     ((stringp (cdr displayed-text))
+      (cdr displayed-text))
+     (t
+      (char-to-string (cdr displayed-text))))))
