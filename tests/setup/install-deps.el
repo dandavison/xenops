273271a02deaf8b30e1bcdbe43c79dbbaa73e2bc
Dan Davison <dandavison7@gmail.com>
2019-12-08 20:36:10 -0500

    Install org-mode >= 9 during tests

--- tests/setup/install-deps.el
+++ tests/setup/install-deps.el
@@ -6,13 +6,16 @@
     dash
     dash-functional
     f
-    s))
+    org
+    s
+    use-package))
 
 (defun xenops-dependencies-setup-package-repositories ()
   (setq package-archives
         (list (if melpa-stable
                   '("melpa-stable" . "https://stable.melpa.org/packages/")
-                '("melpa" . "https://melpa.org/packages/"))))
+                '("melpa" . "https://melpa.org/packages/"))
+              '("org" . "https://orgmode.org/elpa/")))
   (package-refresh-contents))
 
 (defun xenops-package-install (package)
@@ -24,6 +27,9 @@
       (message "%S: ...OK" package))))
 
 (defun xenops-dependencies-check-packages ()
+  (unless (string-match "^9\." org-version)
+    (message "org-version is %S but >= 9 is required" org-version)
+    (kill-emacs 1))
   (save-window-excursion
     (package-list-packages t)
     (condition-case nil
@@ -35,4 +41,5 @@
 
 (xenops-dependencies-setup-package-repositories)
 (mapc #'xenops-package-install xenops-dependencies)
+(use-package org :ensure org-plus-contrib :pin org)
 (xenops-dependencies-check-packages)
