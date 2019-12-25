681dfa6e57a51f43155312a7a30744ef9f179049
Dan Davison <dandavison7@gmail.com>
2019-12-25 07:07:46 +0000

    delete overlays utility

--- xenops-overlay.el
+++ xenops-overlay.el
@@ -3,4 +3,9 @@
 (defun xenops-overlay-at-point ()
   (--first (overlay-get it 'xenops-overlay-type) (overlays-at (point))))
 
+(defun xenops-overlay-delete-overlays (&optional beg end)
+  (interactive "r")
+  (dolist (ov (overlays-in (or beg (point-min)) (or end (point-max))))
+    (delete-overlay ov)))
+
 (provide 'xenops-overlay)
